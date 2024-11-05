fa4c938928758e94365548b1838cc20508bcca7b
Lucy Qiu <lucy1049684100@gmail.com>
2024-11-05 16:42:46 +0700

    blue

--- apps/web/components/GardenBackground.tsx
+++ apps/web/components/GardenBackground.tsx
@@ -7,7 +7,7 @@ import * as THREE from "three";
 import { extend } from "@react-three/fiber";
 import { shaderMaterial } from "@react-three/drei";
 import { MeshStandardMaterial } from "three";
-
+import { colorSets2 } from "../constants/skyColors";
 // Add this type declaration before the component
 declare global {
   namespace JSX {
@@ -255,94 +255,8 @@ export function GardenBackground() {
     const baseGround = "#22D91E";
 
     // Updated color combinations for three-color gradients
-    const colorSets = [
-      {
-        sky1: "#F6D8C1",
-        sky2: "#A17184",
-        sky3: "#CC1D18",
-        sky4: "#4E1C28",
-        ground: baseGround,
-      }, // Midnight 0-1 done
-      {
-        sky1: "#DCDED2",
-        sky2: "#10181C",
-        sky3: "#510A05",
-        sky4: "#678882",
-        ground: baseGround,
-      }, // Late Night 2-3 done
-      {
-        sky1: "#ECDFCF",
-        sky2: "#CC1D18",
-        sky3: "#95A7BC",
-        sky4: "#CBADAD",
-        ground: baseGround,
-      }, // Dawn 4-5 done
-      {
-        sky1: "#FFFFFF",
-        sky2: "#FFF505",
-        sky3: "#879BBB",
-        sky4: "#9D9CB3",
-        ground: baseGround,
-      }, // Sunrise 6-7 done
-      {
-        sky1: "#FEFFF7",
-        sky2: "#ADD19A",
-        sky3: "#E1C8BF",
-        sky4: "#95A6C9",
-        ground: baseGround,
-      }, // Morning 8-9 done
-      {
-        sky1: "#F5F1EB",
-        sky2: "#708B75",
-        sky3: "#CD9265",
-        sky4: "#78AAC2",
-        ground: baseGround,
-      }, // Late Morning 10-11 done
-      {
-        sky1: "#F9F5E8",
-        sky2: "#A6C6B8",
-        sky3: "#EBC4A5",
-        sky4: "#6EC1E6",
-        ground: baseGround,
-      }, // Noon 12-13 done
-      {
-        sky1: "#FFFBED",
-        sky2: "#FEFE91",
-        sky3: "#FE770D",
-        sky4: "#85BCD6",
-        ground: baseGround,
-      }, // Late Afternoon 14-15 done
-      {
-        sky1: "#FEFCE8",
-        sky2: "#FCD2C0",
-        sky3: "#BF2022",
-        sky4: "#42999F",
-        ground: baseGround,
-      }, // Sunset 16-17 done
-      {
-        sky1: "#FFFFFB",
-        sky2: "#FE6E0B",
-        sky3: "#16214F",
-        sky4: "#779599",
-        ground: baseGround,
-      }, // Evening 18-19 done
-      {
-        sky1: "#F6D8C1",
-        sky2: "#A17184",
-        sky3: "#CC1D18",
-        sky4: "#4E1C28",
-        ground: baseGround,
-      }, // Night 20-21 tbd
-      {
-        sky1: "#F6D8C1",
-        sky2: "#A17184",
-        sky3: "#CC1D18",
-        sky4: "#4E1C28",
-        ground: baseGround,
-      }, // Late Night 22-23 tbd
-    ];
 
-    const colors = colorSets[timeSlot]!;
+    const colors = colorSets2[timeSlot]!;
 
     // Blend sky colors with ground
     const skyColor = new THREE.Color(colors.sky2);
