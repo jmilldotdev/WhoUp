5f121f277c077b27fab456ef60b1baf161bcf71b
Jonathan Miller <jon@jmill.dev>
2024-11-06 14:43:37 +0700

    friendships

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,22 @@
 import { redirect } from "next/navigation";
 import { MoonPhaseViewer } from "@/components/MoonPhaseViewer";
 import { getUserProfile } from "@/actions/auth";
+import { UserProvider } from "@/providers/UserProvider";
 
 export default async function Home() {
   const profile = await getUserProfile();
+
+  if (!profile) {
+    redirect("/login");
+  }
 
   if (!profile?.username) {
     redirect("/login/username");
   }
 
-  return <MoonPhaseViewer />;
+  return (
+    <UserProvider userId={profile.user_id} username={profile.username}>
+      <MoonPhaseViewer />
+    </UserProvider>
+  );
 }
--- packages/supabase/src/seed.ts
+++ packages/supabase/src/seed.ts
@@ -17,6 +17,26 @@ const supabaseAdmin = createClient<Database>(
 
 async function seedDatabase() {
   console.log("Seeding database...");
+
+  const { data, error } = await supabaseAdmin.auth.admin.listUsers();
+  const user1 = data?.users[0];
+  const user2 = data?.users[1];
+
+  if (!user1 || !user2 || error) {
+    throw new Error("No users found");
+  }
+
+  // Create a profile for user1
+  await supabase.from("Profiles").insert({
+    user_id: user1.id,
+    username: "user1",
+  });
+
+  // Create a profile for user2
+  await supabase.from("Profiles").insert({
+    user_id: user2.id,
+    username: "user2",
+  });
 }
 
 seedDatabase();
