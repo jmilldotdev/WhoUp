edca442a5775c7654f61338038ae9e6ff7d83c0a
Jonathan Miller <jon@jmill.dev>
2024-11-06 14:52:13 +0700

    Broadcast creation

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,22 +1,5 @@
-import { redirect } from "next/navigation";
 import { MoonPhaseViewer } from "@/components/MoonPhaseViewer";
-import { getUserProfile } from "@/actions/auth";
-import { UserProvider } from "@/providers/UserProvider";
 
 export default async function Home() {
-  const profile = await getUserProfile();
-
-  if (!profile) {
-    redirect("/login");
-  }
-
-  if (!profile?.username) {
-    redirect("/login/username");
-  }
-
-  return (
-    <UserProvider userId={profile.user_id} username={profile.username}>
-      <MoonPhaseViewer />
-    </UserProvider>
-  );
+  return <MoonPhaseViewer />;
 }
