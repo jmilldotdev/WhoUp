d56c7807f0c183d74e2f1c70bc58a7c388619428
Jonathan Miller <jon@jmill.dev>
2024-11-06 07:55:11 +0700

    friendship data model changes

--- packages/supabase/migrations/20241105011047_initial.sql
+++ packages/supabase/migrations/20241105011047_initial.sql
@@ -27,6 +27,15 @@ create table "public"."Connections" (
     "asset_url" text
 );
 
+create table "public"."FriendRequests" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "requesting_user_id" uuid not null,
+    "friend_user_id" uuid not null,
+    "accepted_at" timestamp with time zone,
+    "declined_at" timestamp with time zone
+);
+
 create table "public"."Notifications" (
     "id" uuid not null default gen_random_uuid(),
     "created_at" timestamp with time zone not null default now(),
@@ -39,6 +48,14 @@ create table "public"."Notifications" (
     "action_url" text
 );
 
+create table "public"."Profiles" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "user_id" uuid not null default gen_random_uuid(),
+    "username" text not null,
+    "phone_number" text
+);
+
 create table "public"."PushSubscriptions" (
     "id" uuid not null default gen_random_uuid(),
     "created_at" timestamp with time zone not null default now(),
@@ -49,6 +66,13 @@ create table "public"."PushSubscriptions" (
     "is_active" boolean not null default true
 );
 
+create table "public"."UserFriendships" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "user_id" uuid not null default gen_random_uuid(),
+    "friend_id" uuid not null
+);
+
 CREATE UNIQUE INDEX "BroadcastUsers_pkey" ON public."BroadcastUsers" USING btree (id);
 
 CREATE UNIQUE INDEX "Broadcasts_pkey" ON public."Broadcasts" USING btree (id);
@@ -57,11 +81,17 @@ CREATE UNIQUE INDEX "ConnectionInstance_pkey" ON public."ConnectionInstance" USI
 
 CREATE UNIQUE INDEX "Connections_pkey" ON public."Connections" USING btree (id);
 
+CREATE UNIQUE INDEX "FriendRequests_pkey" ON public."FriendRequests" USING btree (id);
+
 CREATE UNIQUE INDEX "Notifications_pkey" ON public."Notifications" USING btree (id);
+
+CREATE UNIQUE INDEX "Profiles_pkey" ON public."Profiles" USING btree (id);
+
+CREATE UNIQUE INDEX "PushSubscriptions_endpoint_key" ON public."PushSubscriptions" USING btree (endpoint);
 
 CREATE UNIQUE INDEX "PushSubscriptions_pkey" ON public."PushSubscriptions" USING btree (id);
 
-CREATE UNIQUE INDEX "PushSubscriptions_endpoint_key" ON public."PushSubscriptions" USING btree (endpoint);
+CREATE UNIQUE INDEX "UserFriendships_pkey" ON public."UserFriendships" USING btree (id);
 
 alter table
     "public"."BroadcastUsers"
@@ -84,14 +114,29 @@ add
     constraint "Connections_pkey" PRIMARY KEY using index "Connections_pkey";
 
 alter table
+    "public"."FriendRequests"
+add
+    constraint "FriendRequests_pkey" PRIMARY KEY using index "FriendRequests_pkey";
+
+alter table
     "public"."Notifications"
 add
     constraint "Notifications_pkey" PRIMARY KEY using index "Notifications_pkey";
 
 alter table
+    "public"."Profiles"
+add
+    constraint "Profiles_pkey" PRIMARY KEY using index "Profiles_pkey";
+
+alter table
     "public"."PushSubscriptions"
 add
     constraint "PushSubscriptions_pkey" PRIMARY KEY using index "PushSubscriptions_pkey";
+
+alter table
+    "public"."UserFriendships"
+add
+    constraint "UserFriendships_pkey" PRIMARY KEY using index "UserFriendships_pkey";
 
 alter table
     "public"."BroadcastUsers"
@@ -142,6 +187,22 @@ alter table
     "public"."Connections" validate constraint "Connections_user_id_fkey";
 
 alter table
+    "public"."FriendRequests"
+add
+    constraint "FriendRequests_friend_user_id_fkey" FOREIGN KEY (friend_user_id) REFERENCES auth.users(id) not valid;
+
+alter table
+    "public"."FriendRequests" validate constraint "FriendRequests_friend_user_id_fkey";
+
+alter table
+    "public"."FriendRequests"
+add
+    constraint "FriendRequests_requesting_user_id_fkey" FOREIGN KEY (requesting_user_id) REFERENCES auth.users(id) not valid;
+
+alter table
+    "public"."FriendRequests" validate constraint "FriendRequests_requesting_user_id_fkey";
+
+alter table
     "public"."Notifications"
 add
     constraint "Notifications_user_id_fkey" FOREIGN KEY (user_id) REFERENCES auth.users(id) not valid;
@@ -150,12 +211,36 @@ alter table
     "public"."Notifications" validate constraint "Notifications_user_id_fkey";
 
 alter table
+    "public"."Profiles"
+add
+    constraint "Profiles_user_id_fkey" FOREIGN KEY (user_id) REFERENCES auth.users(id) not valid;
+
+alter table
+    "public"."Profiles" validate constraint "Profiles_user_id_fkey";
+
+alter table
     "public"."PushSubscriptions"
 add
     constraint "PushSubscriptions_user_id_fkey" FOREIGN KEY (user_id) REFERENCES auth.users(id) not valid;
 
 alter table
     "public"."PushSubscriptions" validate constraint "PushSubscriptions_user_id_fkey";
+
+alter table
+    "public"."UserFriendships"
+add
+    constraint "UserFriendships_friend_id_fkey" FOREIGN KEY (friend_id) REFERENCES auth.users(id) not valid;
+
+alter table
+    "public"."UserFriendships" validate constraint "UserFriendships_friend_id_fkey";
+
+alter table
+    "public"."UserFriendships"
+add
+    constraint "UserFriendships_user_id_fkey" FOREIGN KEY (user_id) REFERENCES auth.users(id) not valid;
+
+alter table
+    "public"."UserFriendships" validate constraint "UserFriendships_user_id_fkey";
 
 grant delete on table "public"."BroadcastUsers" to "anon";
 
@@ -397,6 +482,66 @@ grant
 update
     on table "public"."Connections" to "service_role";
 
+grant delete on table "public"."FriendRequests" to "anon";
+
+grant
+insert
+    on table "public"."FriendRequests" to "anon";
+
+grant references on table "public"."FriendRequests" to "anon";
+
+grant
+select
+    on table "public"."FriendRequests" to "anon";
+
+grant trigger on table "public"."FriendRequests" to "anon";
+
+grant truncate on table "public"."FriendRequests" to "anon";
+
+grant
+update
+    on table "public"."FriendRequests" to "anon";
+
+grant delete on table "public"."FriendRequests" to "authenticated";
+
+grant
+insert
+    on table "public"."FriendRequests" to "authenticated";
+
+grant references on table "public"."FriendRequests" to "authenticated";
+
+grant
+select
+    on table "public"."FriendRequests" to "authenticated";
+
+grant trigger on table "public"."FriendRequests" to "authenticated";
+
+grant truncate on table "public"."FriendRequests" to "authenticated";
+
+grant
+update
+    on table "public"."FriendRequests" to "authenticated";
+
+grant delete on table "public"."FriendRequests" to "service_role";
+
+grant
+insert
+    on table "public"."FriendRequests" to "service_role";
+
+grant references on table "public"."FriendRequests" to "service_role";
+
+grant
+select
+    on table "public"."FriendRequests" to "service_role";
+
+grant trigger on table "public"."FriendRequests" to "service_role";
+
+grant truncate on table "public"."FriendRequests" to "service_role";
+
+grant
+update
+    on table "public"."FriendRequests" to "service_role";
+
 grant delete on table "public"."Notifications" to "anon";
 
 grant
@@ -457,6 +602,66 @@ grant
 update
     on table "public"."Notifications" to "service_role";
 
+grant delete on table "public"."Profiles" to "anon";
+
+grant
+insert
+    on table "public"."Profiles" to "anon";
+
+grant references on table "public"."Profiles" to "anon";
+
+grant
+select
+    on table "public"."Profiles" to "anon";
+
+grant trigger on table "public"."Profiles" to "anon";
+
+grant truncate on table "public"."Profiles" to "anon";
+
+grant
+update
+    on table "public"."Profiles" to "anon";
+
+grant delete on table "public"."Profiles" to "authenticated";
+
+grant
+insert
+    on table "public"."Profiles" to "authenticated";
+
+grant references on table "public"."Profiles" to "authenticated";
+
+grant
+select
+    on table "public"."Profiles" to "authenticated";
+
+grant trigger on table "public"."Profiles" to "authenticated";
+
+grant truncate on table "public"."Profiles" to "authenticated";
+
+grant
+update
+    on table "public"."Profiles" to "authenticated";
+
+grant delete on table "public"."Profiles" to "service_role";
+
+grant
+insert
+    on table "public"."Profiles" to "service_role";
+
+grant references on table "public"."Profiles" to "service_role";
+
+grant
+select
+    on table "public"."Profiles" to "service_role";
+
+grant trigger on table "public"."Profiles" to "service_role";
+
+grant truncate on table "public"."Profiles" to "service_role";
+
+grant
+update
+    on table "public"."Profiles" to "service_role";
+
 grant delete on table "public"."PushSubscriptions" to "anon";
 
 grant
@@ -516,3 +721,63 @@ grant truncate on table "public"."PushSubscriptions" to "service_role";
 grant
 update
     on table "public"."PushSubscriptions" to "service_role";
+
+grant delete on table "public"."UserFriendships" to "anon";
+
+grant
+insert
+    on table "public"."UserFriendships" to "anon";
+
+grant references on table "public"."UserFriendships" to "anon";
+
+grant
+select
+    on table "public"."UserFriendships" to "anon";
+
+grant trigger on table "public"."UserFriendships" to "anon";
+
+grant truncate on table "public"."UserFriendships" to "anon";
+
+grant
+update
+    on table "public"."UserFriendships" to "anon";
+
+grant delete on table "public"."UserFriendships" to "authenticated";
+
+grant
+insert
+    on table "public"."UserFriendships" to "authenticated";
+
+grant references on table "public"."UserFriendships" to "authenticated";
+
+grant
+select
+    on table "public"."UserFriendships" to "authenticated";
+
+grant trigger on table "public"."UserFriendships" to "authenticated";
+
+grant truncate on table "public"."UserFriendships" to "authenticated";
+
+grant
+update
+    on table "public"."UserFriendships" to "authenticated";
+
+grant delete on table "public"."UserFriendships" to "service_role";
+
+grant
+insert
+    on table "public"."UserFriendships" to "service_role";
+
+grant references on table "public"."UserFriendships" to "service_role";
+
+grant
+select
+    on table "public"."UserFriendships" to "service_role";
+
+grant trigger on table "public"."UserFriendships" to "service_role";
+
+grant truncate on table "public"."UserFriendships" to "service_role";
+
+grant
+update
+    on table "public"."UserFriendships" to "service_role";
